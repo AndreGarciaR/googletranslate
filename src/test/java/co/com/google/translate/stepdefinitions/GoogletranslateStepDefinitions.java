401423f81fa9e6e9e65adf895ecc9079ad066f59
Andrea Garcia Rodriguez <agarciar@qvision.us>
2022-11-29 11:55:33 -0500

    primer proyecto

---
+++ src/test/java/co/com/google/translate/stepdefinitions/GoogletranslateStepDefinitions.java
@@ -0,0 +1,23 @@
+package co.com.google.translate.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoogletranslateStepDefinitions {
+
+    @Given("that Susan wants to translate a word")
+    public void thatSusanWantsToTranslateAWord(){
+
+    }
+
+    @When("she translates the word cheese from English to Spanish")
+    public void sheTranslatesTheWordFromEnglishToSpanish(){
+
+    }
+
+    @Then("she should see the word queso in the screen")
+    public void sheShouldSeeTheWordInTheScreen(){
+
+    }
+}
